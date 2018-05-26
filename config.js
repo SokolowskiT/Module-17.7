ec2c4a87e28837f36e0f068ccc4d3bdf14529689
Tomasz Sokolowski <sokolowskit@gmail.com>
2018-05-26 22:38:00 +0200

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '708980820809-4smm21mr81k5668f5u5r2m12k48um9os.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'KeJEeIovaGj5IWOIluiguqjF',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
